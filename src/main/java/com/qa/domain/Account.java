1e3c4e574423215ca8acd00bc9f62e21c7531a81
AbdiHussein <abdihussein_93@hotmail.com>
2018-04-23 16:22:35 +0100

    2nd commit

--- src/main/java/com/qa/domain/Account.java
+++ src/main/java/com/qa/domain/Account.java
@@ -1,21 +1,17 @@
 package com.qa.domain;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Account {
-	
-	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+
+	@GeneratedValue
+	@Id
 	private Long id;
-	@Column(length = 100)
 	private String firstName;
-	@Column(length = 50)
 	private String secondName;
-	@Column(length = 4)
 	private String accountNumber;
 
 	public Account(String firstName, String secondName, String accountNumber) {
@@ -46,6 +42,14 @@ public class Account {
 
 	public void setAccountNumber(String accountNumber) {
 		this.accountNumber = accountNumber;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 }
